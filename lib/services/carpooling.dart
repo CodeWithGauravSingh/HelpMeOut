fde090e97d3614081d3e04866abe8465af7edd16
poga0204 <pragatigupta.2002@gmail.com>
2023-05-19 14:18:14 +0530

    Search

--- lib/services/carpooling.dart
+++ lib/services/carpooling.dart
@@ -11,28 +11,121 @@ class CarPoolingPage extends StatefulWidget {
 }
 
 class _CarPoolingPageState extends State<CarPoolingPage> {
+  String? src = '';
+  String? dis = '';
+
+  final TextEditingController _srcController = TextEditingController();
+  final TextEditingController _disController = TextEditingController();
+
+  @override
+  void dispose() {
+    _srcController.dispose();
+    _disController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('CAR POOLING'),
-          centerTitle: true,
-          backgroundColor: Color(0xFF68B1D0),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        elevation: 0,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: TextField(
+                controller: _srcController,
+                decoration: InputDecoration(
+                  hintText: 'From',
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    src = value;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: TextField(
+                controller: _disController,
+                decoration:
+                    InputDecoration(hintText: 'To', border: InputBorder.none),
+                onChanged: (value) {
+                  setState(() {
+                    dis = value;
+                  });
+                },
+              ),
+            ),
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Poolrequest(),
-        ),
-        backgroundColor: Color(0xFFE4F4FD),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => pool()))
-          },
-          backgroundColor: Color(0xFF68B1D0),
-        ),
+        centerTitle: false,
+        backgroundColor: const Color(0xFF68B1D0),
+        // actions: [
+        //   IconButton(
+        //     onPressed: () {
+        //       setState(() {
+        //         src = _srcController.text;
+        //         dis = _disController.text;
+        //         print(src);
+        //         print(dis);
+        //       });
+        //     },
+        //     icon: const Icon(Icons.search),
+        //   ),
+        // ],
       ),
+      body: SingleChildScrollView(
+        child: Poolrequest(src: src, dis: dis),
+      ),
+      backgroundColor: Color(0xFFE4F4FD),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => pool()))
+        },
+        backgroundColor: Color(0xFF68B1D0),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          // The search area here
+          title: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 40,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(5)),
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {},
+                    ),
+                    hintText: 'Search...',
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
@@ -71,132 +164,142 @@ class _PoolRequestState extends State<PoolRequest> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(15),
-      child: Card(
-        elevation: 5,
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: EdgeInsets.all(15),
+        child: Card(
+            elevation: 5,
+            color: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.circle, color: Color(0xFF68B1D0)),
+                              SizedBox(width: 10),
+                              Text(
+                                widget.source!,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on, color: Color(0xFF68B1D0)),
+                              SizedBox(width: 10),
+                              Text(
+                                widget.destination!,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Text(
+                        '\u{20B9} ' + widget.fare.toString(),
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(Icons.drive_eta, color: Color(0xFF68B1D0)),
+                      SizedBox(width: 10),
+                      Text(
+                        widget.type!,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Row(
                         children: [
-                          Icon(Icons.circle, color: Color(0xFF68B1D0)),
+                          Icon(Icons.date_range, color: Color(0xFF68B1D0)),
                           SizedBox(width: 10),
-                          Text(widget.source!,
+                          Text(
+                            widget.Date!,
                             style: TextStyle(
                               fontSize: 20,
                             ),
                           )
                         ],
                       ),
-                      SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.location_on, color: Color(0xFF68B1D0)),
+                          Icon(Icons.access_time_outlined,
+                              color: Color(0xFF68B1D0)),
                           SizedBox(width: 10),
-                          Text(widget.destination!,
+                          Text(
+                            widget.Time!,
                             style: TextStyle(
                               fontSize: 20,
                             ),
                           )
                         ],
-                      )
+                      ),
                     ],
                   ),
-                  Text(
-                    '\u{20B9} '+widget.fare.toString(),
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w500,
+                  SizedBox(height: 15),
+                  ListTile(
+                    leading: Image.asset('assets/avataricon.png'),
+                    title: Text(
+                      widget.name!,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Icon(Icons.drive_eta, color: Color(0xFF68B1D0)),
-                  SizedBox(width: 10),
-                  Text(widget.type!,
-                    style: TextStyle(
-                      fontSize: 20,
+                    subtitle: Text(
+                      widget.contact!,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    trailing: InkWell(
+                      child: Icon(
+                        Icons.call,
+                        size: 30,
+                        color: Colors.green,
+                      ),
+                      onTap: () {
+                        _makingPhoneCall();
+                      },
                     ),
                   )
                 ],
               ),
-              SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.date_range, color: Color(0xFF68B1D0)),
-                      SizedBox(width: 10),
-                      Text(widget.Date!,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.access_time_outlined, color: Color(0xFF68B1D0)),
-                      SizedBox(width: 10),
-                      Text(widget.Time!,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 15),
-              ListTile(
-                leading: Image.asset('assets/avataricon.png'),
-                title: Text(widget.name!,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                subtitle: Text(widget.contact!,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                trailing: InkWell(
-                  child: Icon(
-                    Icons.call,
-                    size: 30,
-                    color: Colors.green,
-                  ),
-                  onTap: () {
-                    _makingPhoneCall();
-                  },
-                ),
-              )
-            ],
-          ),
-        )
-      )
-    );
+            )));
   }
 }
 
 class Poolrequest extends StatefulWidget {
-  const Poolrequest({Key? key}) : super(key: key);
+  final String? src;
+  final String? dis;
+
+  const Poolrequest({Key? key, this.src, this.dis}) : super(key: key);
 
   @override
-  State<Poolrequest> createState() => _PoolrequestState();
+  _PoolrequestState createState() => _PoolrequestState();
 }
 
 class _PoolrequestState extends State<Poolrequest> {
@@ -214,6 +317,27 @@ class _PoolrequestState extends State<Poolrequest> {
             snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasError) {
             return const Text('Error');
+          } else if (widget.src!.isNotEmpty && widget.dis!.isNotEmpty) {
+            List<Widget> feedlist =
+                snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+                  document.data() as Map<String, dynamic>;
+              if (data['source'] == widget.src &&
+                  data['destination'] == widget.dis) {
+                return PoolRequest(
+                  source: data['source'],
+                  destination: data['destination'],
+                  Date: data['date'],
+                  Time: data['time'],
+                  type: data['type'],
+                  contact: data['contact'],
+                  name: data['name'],
+                  fare: data['fare'],
+                );
+              }
+              return SizedBox.shrink(); // or `return null;`
+            }).toList();
+            return Column(children: feedlist);
           } else if (snapshot.hasData) {
             List<Widget> feedlist =
                 snapshot.data!.docs.map((DocumentSnapshot document) {
